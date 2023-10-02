dae19ca35e869d874aa284b8c692c803447c41d2
lidiamoskovenko <130401685+lidiamoskovenko@users.noreply.github.com>
2023-10-02 16:42:01 +0200

    start

--- src/js/render/render-coctails-cards.js
+++ src/js/render/render-coctails-cards.js
@@ -1,104 +1,46 @@
 // написати функцію яка буде відмальовувати картки коктейлів. З файла user-screen-width-follower.js взяти кількість карток, які необхідно намалювати, відповідно до ширини екрану. В параметри функції буде приходити масив з об'єктами коктейлів та посилання на елемент в якому ці картки необхідно намалювати. Зробити іменований експорт цієї функції, для її перевикористання. у разі якщо довжина масиву більша за число карток, які можна намалювати, відповідно до ширини екрану, викликати функцію пагінації.Картки мають бути тегами li, тому що малюватись вони будуть в тезі ul
-// let query ='';
-
-// const BASE_URL = 'https://drinkify.b.goit.study/api/v1/cocktails/search/';
-
-// function fetchImage() {
-// if(query.length===1){
-//     return fetch(`${BASE_URL}?f=${query}`)
-//     .then(response => {
-//       return response.json();
-//     })
-// }
-//     return fetch(`${BASE_URL}?s=${query}`)
-//       .then(response => {
-//         return response.json();
-//       })
-//       .catch(error => {
-//         throw new Error;
-//       });
-//   }
-
-// const searchForm = document.querySelector('.search__form');
 const cocktailsList = document.querySelector('.cocktails__list');
-const titleRemove = document.querySelector('.search__title');
 const learnMoreBtn = document.querySelectorAll('.learnmore__btn');
 const svg = document.querySelectorAll('.cocktails__svg');
-
-const itemBtnContainer = document.querySelectorAll(
-  '.cocktails__btn__container'
-);
-
-// titleRemove.style.display = 'none';
-
-// searchForm.addEventListener('submit', createImgCards);
-
-// if (itemBtnContainer) {
-//   itemBtnContainer.forEach(button => {
-//   button.addEventListener('click', changeEvents);
-// })};
-
-// async function createImgCards(event) {
-// event.preventDefault();
-// query = event.currentTarget.elements.query.value.trim();
-
-// clearCocktailsList();
-
-// try {
-// const data = await fetchImage();
-// data.length = renderingCardDependency();
-// titleRemove.style.display = 'block';
-// renderCocktailsList(data);
-// }
-// catch (error) {
-// console.log(error.message);
-// }
-// }
-
-export function renderCocktailsList(images, container) {
-  container.innerHTML = images
-    .map(
-      image => `
-<li class="cocktails__item change-theme" id = ${image._id}>
-<img class="cocktails__item__img" loading="lazy" src=${image.drinkThumb} alt=${image.drink}>
+function renderCocktailsList(images) {
+cocktailsList.innerHTML += images.map(image => `
+<li class="cocktails__item" id = ${image._id}>
+<img class="cocktails__item__img" src=${image.drinkThumb} alt=${image.drink}>
 <h3 class="cocktails__item__header">${image.drink}</h3>
 <p class="cocktails__item__description">${image.description}</p>
 <div class="cocktails__btn__container">
 <button type="button" class="learnmore__btn">learn more</button>
 <button type="button"  class="svg__btn"> 
  <svg class="svg-heart">
-<use class="cocktails__svg add_favorites_js" href="./img/sprite.svg#icon-heart"></use>
+<use class="cocktails__svg add_favorites_js" href="img/sprite.svg#icon-heart"></use>
 </svg>
 </button>
 </div> 
 </li>
-`
-    )
-    .join('');
-}
-function renderingCardDependency() {
-  let widthWindowUser = document.documentElement.clientWidth;
-  let requestCards = widthWindowUser > 1280 ? 9 : 8;
-  return requestCards;
+`)
+.join('');
 }
 
-function clearCocktailsList() {
-  cocktailsList.innerHTML = '';
-}
 
-function changeEvents(event) {
-  const target = event.target;
-  if (target.classList.contains('learnmore__btn')) {
-    console.log('Open modal');
+
+cocktailsList.addEventListener('click', changeEvents);
+
+
+function changeEvents(event){
+    const target = event.target;
+    if (target.classList.contains('learnmore__btn')) {
+    console.log("Open modal");
+
   } else if (target.classList.contains('svg__btn')) {
-    addToFavorites();
-    if (addToFavorites) {
-      svg.classList.remove('.add_favorites_js');
-      svg.classList.add('.remove_favorites_js');
-    }
-    console.log('Add to favorites');
+    addToFavorites(event);
+    if(addToFavorites){
+svg.classList.remove('.add_favorites_js');
+svg.classList.add('.remove_favorites_js');
+    }}
+    
+    return;
   }
-}
+// LOCAL STORAGE GET AND SAVE
 
 function getFavorites() {
   const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
@@ -109,19 +51,78 @@ function saveFavorites(favorites) {
 }
 
 function addToFavorites(event) {
-  const card = event.target.parentElement;
+  const card = event.currentTarget.parentElement;
   const cocktailData = {
-    id: card.querySelector('#id').textContent,
+    id:card.querySelector('#id').textContent,
     name: card.querySelector('.cocktails__item__header').textContent,
-    description: card.querySelector('.cocktails__item__description')
-      .textContent,
+    description: card.querySelector('.cocktails__item__description').textContent,
     imageSrc: card.querySelector('.cocktails__item__img').src,
   };
 
   const favorites = getFavorites();
 
   if (!favorites.includes(cocktailData)) {
-    favorites.push(cocktailData);
+      favorites.push(cocktailData);
   }
   saveFavorites(favorites);
 }
+
+
+// FAVORITES COCKTAILS
+
+const favorList = document.querySelector('.favor_list');
+
+function renderFavorCocktailsList(favors) {
+  const favors = getFavorites();
+
+  if(!favors){
+    console.log('not cocktailes')
+  }
+
+  favorList.innerHTML += favors.map(favor => `
+  <li class="favor__item" id = ${favor.id}>
+  <img class="favor__item__img" src=${favor.imageSrc} alt=${favor.name}>
+  <h3 class="favor__item__header">${favor.name}</h3>
+  <p class="favor__item__description">${favor.description}</p>
+  <div class="favor__btn__container">
+  <button type="button" class="learnmore__btn">learn more</button>
+  <button type="button"  class="trash__btn"> 
+   <svg class="svg-icon">
+  <use class="cocktails__svg add_favorites_js" href="img/sprite.svg#icon-trash"></use>
+  </svg>
+  </button>
+  </div> 
+  </li>
+  `)
+  .join('');
+  }
+
+  favorList.addEventListener('click', function(event) {
+    const target = event.target;
+    if (target.classList.contains('learnmore__btn')) {
+      console.log("Open modal");
+    } else if (target.classList.contains('trash__btn')) {
+      removeFromFavor(event);
+    }
+  });  
+  
+  function removeFromFavor(event){
+    const favorcard = event.currentTarget.parentElement;
+    const favorCocktailData = {
+      id:favorcard.querySelector('#id').textContent,
+      name: favorcard.querySelector('.cocktails__item__header').textContent,
+      description: favorcard.querySelector('.cocktails__item__description').textContent,
+      imageSrc: favorcard.querySelector('.cocktails__item__img').src,
+    };
+    localStorage.removeItem(favorCocktailData);
+
+    const favorites = getFavorites();
+
+    saveFavorites(favorites); 
+    renderFavorCocktailsList(favorites);
+  }
+  
+
+
+
+
